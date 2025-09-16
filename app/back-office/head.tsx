3a39809c5af2d326657f95d409dc8521cff3e840
SPST-WINE <spediamoallestero@gmail.com>
2025-09-16 18:40:54 +0200

    Update head.tsx

--- app/back-office/head.tsx
+++ app/back-office/head.tsx
@@ -1,7 +1,20 @@
+// app/back-office/head.tsx
 export default function Head() {
   return (
     <>
-      <meta name="robots" content="noindex" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      {/* CSS locali (niente link a spst-logistics per i CSS) */}
+      <link rel="stylesheet" href="/back-office/base.css?v=1" />
+      <link rel="stylesheet" href="/back-office/quotes-admin.css?v=1" />
+      <style>{`
+        /* opzionale: nasconde qualsiasi dump di debug residuo */
+        body > pre, .debug-dump { display:none !important; }
+      `}</style>
     </>
   );
 }
