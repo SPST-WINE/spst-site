d91e54b45d38085d1cab32b234c438d54c73e0ea
SPST-WINE <spediamoallestero@gmail.com>
2025-09-17 17:26:18 +0200

    Update layout.tsx

--- app/back-office/layout.tsx
+++ app/back-office/layout.tsx
@@ -1,5 +1,6 @@
 // app/back-office/layout.tsx
 import type { Metadata } from 'next';
+import './bo.css'; // ⬅️ importa gli stili del Back Office
 
 export const metadata: Metadata = {
   title: 'SPST Back Office',
