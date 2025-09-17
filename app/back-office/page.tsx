0f3927208c10fa3c9ca5791a11b9af90095ce097
SPST-WINE <spediamoallestero@gmail.com>
2025-09-17 10:57:33 +0200

    Update page.tsx

--- app/back-office/page.tsx
+++ app/back-office/page.tsx
@@ -1,9 +1,17 @@
-// app/back-office/page.tsx
-export default function BackOfficePage() {
-  return (
-    <main>
-      {/* ...markup della pagina... */}
-      <script type="module" src="/bo-assets/esm/main.js" crossOrigin="anonymous"></script>
-    </main>
-  );
-}
+import { ensureBoAssets } from '/bo-assets/esm/main.js';
+
+window.BACK_OFFICE_CONFIG = { PROXY_BASE: '/api/airtable' };
+
+<main className="wrap">
+    <brandbar />
+    <header />
+    <nav />
+    <section id="view-spedizioni">
+        <div id="list"></div>
+    </section>
+    <section id="view-preventivi">
+        <!-- admin forms qui -->
+    </section>
+    <script type="module" src="/bo-assets/esm/main.js" crossOrigin="anonymous" suppressHydrationWarning></script>
+    {ensureBoAssets()}
+</main>
