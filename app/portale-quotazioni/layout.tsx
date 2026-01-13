cf06763ce71eef01404758227fe8f7359c4854bd
SPST - Wine Connect <spediamoallestero@gmail.com>
2026-01-13 11:38:46 +0100

    Update layout.tsx

--- app/portale-quotazioni/layout.tsx
+++ app/portale-quotazioni/layout.tsx
@@ -1,4 +1,10 @@
 // app/portale-quotazioni/layout.tsx
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Richiedi un preventivo - SPST Spedizioni di Vino",
+};
+
 export default function PortaleQuotazioniLayout({
   children,
 }: {
